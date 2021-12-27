4d8be511b2dfc516ad3fef32f99b9553701fa00e
Tuba DEMIR <tubademir23@gmail.com>
2021-12-27 19:37:45 +0300

    add repository with querydsl

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# HMB
-HMB Project Task
-Step 1:
-https://start.spring.io/ download spring project with dependencies: PostgreSql and JPA
-With JPA; entity create the database tables if necessary
+# About
+This project includes simple CRUD operation with one-to-many relation.
+With REST-API the backend developed by Spring Boot.
+Base entities: User and Todo. Aim ise to add/delete/update/list user and todos, also add todo to users.
+# 📝 Get spring empty project
+Bu using [`Spring.io`](https://start.spring.io), firstly create empty spring boot application with dependencies: PostgreSql and JPA
+	With Hibernate; during application starts, entities will be created on database tables if necessary. Key: hibernate-auto
+# 📈 QueryDSL 
+Here is sample usages for this library: [`QueryDSL`](https://www.baeldung.com/rest-api-search-querydsl-web-in-spring-data-jpa)
---
+++ src/main/java/hmb/com/tr/mulakat/user/repository/AppUserRepository.java
@@ -0,0 +1,30 @@
+package hmb.com.tr.mulakat.user.repository;
+
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.SingleValueBinding;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
+
+import hmb.com.tr.mulakat.user.entity.AppUser;
+import hmb.com.tr.mulakat.user.entity.QAppUser;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users", exported = true)
+public interface AppUserRepository
+		extends
+			QuerydslPredicateExecutor<AppUser>,
+			PagingAndSortingRepository<AppUser, Long>,
+			QuerydslBinderCustomizer<QAppUser> {
+
+	default void customize(QuerydslBindings bindings, QAppUser obj) {
+		// Exclude id from filtering
+		bindings.excluding(obj.id);
+		// Make case-insensitive 'like' filter for all string properties
+		bindings.bind(String.class).first(
+				(SingleValueBinding<StringPath, String>) StringExpression::containsIgnoreCase);
+	}
+}
