1e8dd59fd4529fba39c5f7e78ee2fe96c4b7c106
Tuba DEMIR <tubademir23@gmail.com>
2021-12-27 18:50:28 +0300

    first commit Spring

---
+++ src/main/java/hmb/com/tr/mulakat/MulakatApplication.java
@@ -0,0 +1,13 @@
+package hmb.com.tr.mulakat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulakatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulakatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hmb/com/tr/mulakat/MulakatApplicationTests.java
@@ -0,0 +1,13 @@
+package hmb.com.tr.mulakat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulakatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
